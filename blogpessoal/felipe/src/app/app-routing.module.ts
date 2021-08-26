5d786f63c00ccbf35c6b8dc4018a7ac85072f1e0
Felipe <kendyhonda@hotmail.com>
2021-08-26 00:26:11 -0300

    Adicionando as srotas para o blog pessoal, tenho que arrumas as paginas do login que stao bugadas

--- blogpessoal/felipe/src/app/app-routing.module.ts
+++ blogpessoal/felipe/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes =
+[
+  {path:'',redirectTo:'login', pathMatch:'full'},
+  {path:'login',component: LoginComponent},
+  {path:'cadastrar',component: CadastrarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blogpessoal/felipe/src/app/cadastrar/cadastrar.component.spec.ts
+++ blogpessoal/felipe/src/app/cadastrar/cadastrar.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { CadastrarComponent } from './cadastrar.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('CadastrarComponent', () => {
+  let component: CadastrarComponent;
+  let fixture: ComponentFixture<CadastrarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ CadastrarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(CadastrarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
